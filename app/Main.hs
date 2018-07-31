39cf1509c6d4b94714b9a3d47d5f5e305c76dc34
Niklas Hambüchen <mail@nh2.me>
2018-07-31 12:21:33 +0200

    Basic flying

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Concurrent.Async
+-- import Data.Char (ord)
+-- import qualified Data.ByteString as BS
+-- import qualified Data.ByteString.Char8 as BS8
 import Data.IORef
 import qualified Data.Text as T
 import Data.Maybe (catMaybes)
@@ -20,7 +25,7 @@ import qualified Graphics.UI.GLUT as GLUT
 import SpatialMath ( Euler(..), rotateXyzAboutZ, rotVecByEulerB2A )
 import Vis
 
-import System.Clock
+-- import System.Clock
 import System.IO
 import System.Hardware.Serialport
 
@@ -37,6 +42,10 @@ data PlayerState = Running (V3 Double) (V3 Double) (Euler Double)
 data GameState = GameState { playerState :: PlayerState
                            , keySet :: Set.Set Key
                            , lastMousePos :: Maybe (GLint,GLint)
+                           , copterPos :: V3 Double
+                           , deviceAccel :: V3 Int
+                           , accel :: V3 Double
+                           , speed :: V3 Double
                            }
 
 toVertex :: (Real a, Fractional b) => V3 a -> Vertex3 b
@@ -49,14 +58,41 @@ setCamera (Running (V3 x y z) _ euler) =
     xyz0 = V3 x y (z-faceHeight)
     target = xyz0 + rotVecByEulerB2A euler (V3 1 0 0)
 
+initialCopterPos :: V3 Double
+initialCopterPos = V3 0 0 (-1) -- start above the ground
+
 simfun :: Float -> GameState -> IO GameState
-simfun _ (GameState (Running pos _ euler0@(Euler yaw _ _)) keys lmp) = do
+simfun _ (GameState (Running pos _ euler0@(Euler yaw _ _)) keys lmp copterPos deviceAccel accel speed) = do
   Size x y <- GLUT.get GLUT.windowSize
   let x' = (fromIntegral x) `div` 2
       y' = (fromIntegral y) `div` 2
 
   when (Just (x',y') /= lmp) (GLUT.pointerPosition $= (Position x' y'))
-  return $ GameState (Running (pos + (ts *^ v)) v euler0) keys (Just (x',y'))
+
+  let flipX (V3 x y z) = V3 (-x) y z
+
+  let newAccel = (-thrust) *^ accel + V3 0 0 (0.0002)
+  let newSpeed
+        | reset = V3 0 0 0
+        | otherwise = speed + flipX newAccel
+  let newCopterPos
+        | reset = initialCopterPos
+        | otherwise = copterPos + newSpeed
+
+  let speedCap = 0.05
+  let capComponent c
+        | c < -speedCap = -speedCap
+        | c >  speedCap =  speedCap
+        | otherwise = c
+
+  let finalSpeed
+        | reset = V3 0 0 0
+        | otherwise = capComponent <$> newSpeed
+  let finalCopterPos
+        | reset = initialCopterPos
+        | otherwise = newCopterPos
+
+  return $ GameState (Running (pos + (ts *^ v)) v euler0) keys (Just (x',y')) finalCopterPos deviceAccel accel finalSpeed
   where
     v = rotateXyzAboutZ (V3 (w-s) (d-a) 0) yaw
       where
@@ -65,6 +101,11 @@ simfun _ (GameState (Running pos _ euler0@(Euler yaw _ _)) keys lmp) = do
         s = if Set.member (Char 's') keys then 3 else 0
         d = if Set.member (Char 'd') keys then 3 else 0
 
+    reset = Set.member (Char 'r') keys
+
+    -- thrust = if Set.member (Char ' ') keys then 0.005 else 0
+    thrust = if Set.member (Char ' ') keys then 0.5 else 0
+
 keyMouseCallback :: GameState -> Key -> KeyState -> Modifiers -> Position -> GameState
 keyMouseCallback state0 key keystate _ _
   | keystate == Down = state0 {keySet = Set.insert key (keySet state0)}
@@ -72,7 +113,7 @@ keyMouseCallback state0 key keystate _ _
   | otherwise        = state0
 
 motionCallback :: Bool -> GameState -> Position -> GameState
-motionCallback _ state0@(GameState (Running pos v (Euler yaw0 pitch0 _)) _ lmp) (Position x y) =
+motionCallback _ state0@(GameState (Running pos v (Euler yaw0 pitch0 _)) _ lmp _ _ _ _) (Position x y) =
   state0 {playerState = newPlayerState, lastMousePos = Just (x,y)}
   where
     (x0,y0) = case lmp of Nothing -> (x,y)
@@ -96,17 +137,16 @@ data Accel =
     } deriving (Eq, Ord, Show)
 
 
-drawfun :: GameState -> Accel -> VisObject Double
-drawfun (GameState (Running _ _ _) _ _) (Accel x y z) =
-  VisObjects $ [axes,box, plane]
+drawfun :: GameState -> VisObject Double
+drawfun GameState{ playerState = Running _ _ _, copterPos, accel = V3 x y _z } =
+  VisObjects $ [axes, box, plane]
   where
-    x' = -1
     axes = Axes (0.5, 15)
     box =
-      Trans (V3 0 0 x') $
+      Trans copterPos $
         RotEulerDeg (Euler{ eYaw = 0
-                          , ePitch = fromIntegral (-x) * 0.9
-                          , eRoll = fromIntegral (-y) * 0.9
+                          , ePitch = (-x) * 0.9 / 0.00001
+                          , eRoll = (-y) * 0.9 / 0.00001
                           }) $
           Box (0.2, 0.2, 0.2) Solid (makeColor 0 1 1 1)
     plane = Plane (V3 0 0 1) (makeColor 1 1 1 1) (makeColor 0.4 0.6 0.65 0.4)
@@ -116,37 +156,81 @@ main :: IO ()
 main = do
   -- let port = "COM3"           -- Windows
   let port = "/dev/ttyUSB0"   -- Linux
+  -- s <- openSerial port defaultSerialSettings
   h <- hOpenSerial port defaultSerialSettings
     { timeout = 1000
     , commSpeed = CS115200
     }
 
-  accelRef <- newIORef $ Accel 0 0 0
+  deviceAccelRef <- newIORef $ V3 0 0 0
 
   let serialReaderThread = forever $ do
-        h <- hGetLine h
-        case catMaybes $ map (readMaybe . T.unpack) $ T.splitOn " " $ T.strip (T.pack h) of
-          [x, y, z] -> do
-            -- writeIORef accelRef (Accel x y z)
-            nanos <- toNanoSecs <$> getTime Monotonic
-            old <- atomicModifyIORef' accelRef $ \(Accel oldX oldY oldZ) ->
-              let w = 0.2
-                  i old new = floor (fromIntegral old * (1-w) + fromIntegral new * w :: Double)
-                  a = Accel (i oldX x) (i oldY y) (i oldZ z)
-              in (a, a)
-            print (nanos `quot` 1000000)
-            print old
-            print (x, y, z)
+        -- -- recv s 1 >>= (\([c]) -> print (ord c)) . BS8.unpack
+        -- x <- fromIntegral . (\([c]) -> ord c) . BS8.unpack <$> recv s 1
+        -- print x
+
+        -- case [x-100, 0, 0] of
+        --   [x, y, z] -> do
+        --     writeIORef deviceAccelRef (Accel x y z)
+        --     nanos <- toNanoSecs <$> getTime Monotonic
+        --     -- old <- atomicModifyIORef' deviceAccelRef $ \(Accel oldX oldY oldZ) ->
+        --     --   let w = 0.2
+        --     --       i old new = floor (fromIntegral old * (1-w) + fromIntegral new * w :: Double)
+        --     --       a = Accel (i oldX x) (i oldY y) (i oldZ z)
+        --     --   in (a, a)
+        --     -- print (nanos `quot` 1000000)
+        --     -- print (nanos `quot` 1000)
+        --     -- print old
+        --     print (x, y, z)
+        --   _ -> return ()
+
+        -- print "here"
+        l <- hGetLine h
+        -- print l
+        case catMaybes $ map (readMaybe . T.unpack) $ T.splitOn " " $ T.strip (T.pack l) of
+          -- [x, y, z :: Int] -> do
+          x:y:(z :: Int):_ -> do
+            writeIORef deviceAccelRef (V3 x y z)
+            -- nanos <- toNanoSecs <$> getTime Monotonic
+            -- old <- atomicModifyIORef' deviceAccelRef $ \(V3 oldX oldY oldZ) ->
+            --   let w = 0.2
+            --   -- let w = 1
+            --       i old new = floor (fromIntegral old * (1-w) + fromIntegral new * w :: Double)
+            --       a = Accel (i oldX x) (i oldY y) (i oldZ z)
+            --   in (a, a)
+            -- print old
+            -- print (nanos `quot` 1000000)
+            -- print (x, y, z)
           _ -> return ()
 
 
   race_ serialReaderThread $ do
 
-    let state0 = GameState (Running (V3 (-2) 0 0) 0 (Euler 0 0 0)) (Set.empty) Nothing
-        setCam (GameState x _ _) = setCamera x
-        drawfun' x = do
-          accel <- readIORef accelRef
-          return (drawfun x accel, Just None)
+    let state0 =
+          GameState
+            { playerState = Running (V3 (-2) 0 0) 0 (Euler 0 0 0)
+            , keySet = Set.empty
+            , lastMousePos = Nothing
+            , copterPos = initialCopterPos
+            , deviceAccel = V3 0 0 0
+            , accel = V3 0 0 0
+            , speed = V3 0 0 0
+            }
+
+        setCam GameState{ playerState } = setCamera playerState
+
+        simfun' time gameState@GameState{ accel } = do
+          deviceAccel <- readIORef deviceAccelRef
+
+          let interpolate w a b = (1-w) *^ a + w *^ b
+          let w = 0.2
+          let newAccel = interpolate w accel (0.00001 *^ (fromIntegral <$> deviceAccel))
+
+          simfun time gameState{ deviceAccel, accel = newAccel }
+
+        drawfun' :: GameState -> IO (VisObject Double, Maybe Cursor)
+        drawfun' x = return (drawfun x, Just None)
+
     _ <- initThreads
-    playIO (defaultOpts {optWindowName = "play test"}) ts state0 drawfun' simfun setCam
+    playIO (defaultOpts {optWindowName = "play test"}) ts state0 drawfun' simfun' setCam
       (Just keyMouseCallback) (Just (motionCallback True)) (Just (motionCallback False))
